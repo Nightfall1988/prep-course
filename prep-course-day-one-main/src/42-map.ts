9ae12c8f5185e0a91943ed0cc1c68efec96d2c38
Nightfall1988 <grigorijs.mamilovs@gmail.com>
2020-12-13 02:03:58 +0000

    task 42

--- prep-course-day-one-main/src/42-map.ts
+++ prep-course-day-one-main/src/42-map.ts
@@ -1,16 +1,20 @@
 export {};
 
-function tidyUpString(str) {
+function tidyUpString(str:string) {
   return str
     .trim()
     .toLowerCase()
     .replace("/", "");
 }
 
-// You are allowed to edit this function
-function capitalise(str) {}
+function capitalise(arr:string[]) {
+  var newArr =[]
+  newArr = arr.map(name => tidyUpString(name));
+  newArr = newArr.map(name => name.charAt(0).toUpperCase() + name.substr(1))
+  return newArr;
+}
 
 const mentors = ["/Daniel ", "irina ", " Gordon", "ashleigh "];
-let mentorsTidy; // You are allowed to edit this line
+var mentorsTidy = capitalise(mentors); // You are allowed to edit this line
 
 console.log(mentorsTidy); // Expected output: ["Daniel", "Irina", "Gordon", "Ashleigh"]
